be72a1698a0bf7b5e48921cf2e599d7fe3169965
19thyneb <thyne.boonmark@gmail.com>
2021-02-25 13:23:45 -0800

    Removed widget_install.sh
    Enabling lux-widget jupyter extension in postBuild

--- widget_install.sh
+++
@@ -1,6 +0,0 @@
-#npm install # run only for the first time 
-npx webpack # builds tsx --> ts --> js
-#pip install . # builds python wheel and copies relevant js files to luxwidget/
-# When rebuild, these two lines don't need to be rerun since the code is symlinked
-jupyter nbextension install --sys-prefix --symlink --overwrite --py luxwidget
-jupyter nbextension enable --sys-prefix --py luxwidget
