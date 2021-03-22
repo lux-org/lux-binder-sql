4ada8825ad5e6a54b5e151d33be203c851ec31d8
19thyneb <thyneboonmark@berkeley.edu>
2021-03-22 11:06:28 -0700

    Added Script to populate Postgres DB
    Updated PostBuild to run the data uploading scripts

---
+++ scripts/upload_data.py
@@ -0,0 +1,10 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+engine = create_engine("postgresql://testuser:testpass@localhost:5432/testdb")
+
+data = pd.read_csv('https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/college.csv')
+data.to_sql(name='college', con=engine, if_exists = 'replace', index=False)
+
+data = pd.read_csv('https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/car.csv')
+data.to_sql(name='car', con=engine, if_exists = 'replace', index=False)
