66112ce4f0ef11e3db97f66f892eed049c5807aa
Sophia Huang <6860749+sophiahhuang@users.noreply.github.com>
2021-04-07 09:46:57 +0800

    Added employee to upload_data

--- scripts/upload_data.py
+++ scripts/upload_data.py
@@ -8,3 +8,6 @@ data.to_sql(name='college', con=engine, if_exists = 'replace', index=False)
 
 data = pd.read_csv('https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/car.csv')
 data.to_sql(name='car', con=engine, if_exists = 'replace', index=False)
+
+data = pd.read_csv('https://raw.githubusercontent.com/lux-org/lux-datasets/master/data/employee.csv')
+data.to_sql(name='employee', con=engine, if_exists = 'replace', index=False)
